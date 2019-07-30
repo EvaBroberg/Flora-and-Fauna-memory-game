68a3cd3578b899cef9964c51a44a5aad0f9c77f7
Ieva Broberg <ievabroberg@gmail.com>
2019-07-30 23:32:51 +0100

    made cards stick when match and flip back when not

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const cards = document.querySelectorAll('.card');
 
+let flippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     // console.log('clicked');
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    
+    if(!flippedCard) {
+        flippedCard = true;
+        firstCard = this;
+        // console.log(flippedCard);
+        // console.log(firstCard);
+    } else {
+        flippedCard = false;
+        secondCard = this;
+        // console.log(flippedCard, secondCard);
+        // console.log(firstCard.dataset.picture);
+        // console.log(secondCard.dataset.picture);
+        
+        if (firstCard.dataset.picture === secondCard.dataset.picture){
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        }else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 800);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
