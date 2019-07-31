371bebec00fdda3b8e7ac3a8d49475465a8b3fba
Ieva Broberg <ievabroberg@gmail.com>
2019-07-31 22:31:56 +0100

    cleaned up js code by creating separate functions instead of nesting

--- script.js
+++ script.js
@@ -4,31 +4,42 @@ let flippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    // console.log('clicked');
     this.classList.add('flip');
     
     if(!flippedCard) {
         flippedCard = true;
         firstCard = this;
-        // console.log(flippedCard);
-        // console.log(firstCard);
-    } else {
-        flippedCard = false;
-        secondCard = this;
-        // console.log(flippedCard, secondCard);
-        // console.log(firstCard.dataset.picture);
-        // console.log(secondCard.dataset.picture);
         
-        if (firstCard.dataset.picture === secondCard.dataset.picture){
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        }else {
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 800);
-        }
-    }
+        return;
+    } 
+    flippedCard = false;
+    secondCard = this;
+    
+    checkForMatch(); 
+}
+
+
+function checkForMatch() {
+    let match = firstCard.dataset.picture === secondCard.dataset.picture;
+    
+    match ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 800);
+}
+
+function resetBoard(){
+    flipCard, lockBoard = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
